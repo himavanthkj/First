e807aa13c9c44a1babc80eb394909d69ec53482a
himavanthkj@gmail.com <himavanthkj@gmail.com>
2019-07-26 01:55:17 -0400

    Adding scripts sample.sh tomcatrestart.sh tomcatrestart1.sh

---
+++ sample.sh
@@ -0,0 +1,37 @@
+hostip=`hostname -i`
+tomcat_pid() {
+  echo `ps aux | grep org.apache.catalina.startup.Bootstrap | grep -v grep | awk '{ print $2 }'`
+}
+status(){
+	PID=$(tomcat_pid)
+	if [ -n $PID ]
+	then
+		echo "Tomcat has  been started and running"
+	else
+		echo "Tomcat is not running after a start, please have a look into it"
+	fi
+	}
+US(){
+	upid=$(tomcat_pid)
+	urlstatus=`curl -IsS http://${hostip}:8080 | head -1 | awk '{print $2}'`
+	if [ $urlstatus == 200 ]
+	then
+		echo "Tomcat URL is loading with (pid: $upid)"
+	else
+		/opt/tomcat/latest/bin/startup.sh
+		wait $!
+		status
+	fi
+	}
+tomcat_pid
+pid=$(tomcat_pid)
+if [ -n "$pid" ]
+then
+	echo "tomcat is running with (pid: $pid)"
+	US
+else
+	echo "Starting tomcat as its not running"
+	/opt/tomcat/latest/bin/startup.sh
+	wait $!
+	status
+fi
---
+++ tomcatrestart1.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+SHUTDOWN_WAIT=5
+
+tomcat_pid() {
+  echo `ps aux | grep org.apache.catalina.startup.Bootstrap | grep -v grep | awk '{ print $2 }'`
+}
+
+start() {
+  pid=$(tomcat_pid)
+  if [ -n "$pid" ] 
+  then
+    echo "Tomcat is already running (pid: $pid)"
+  else
+    # Start tomcat
+    echo "Starting tomcat"
+    /bin/sh /opt/tomcat/latest/bin/startup.sh
+  fi
+
+
+  return 0
+}
+
+stop() {
+  pid=$(tomcat_pid)
+  if [ -n "$pid" ]
+  then
+    echo "Stoping Tomcat"
+    /bin/sh  /opt/tomcat/latest/bin/shutdown.sh
+
+    let kwait=$SHUTDOWN_WAIT
+    count=0;
+    until [ `ps -p $pid | grep -c $pid` = '0' ] || [ $count -gt $kwait ]
+    do
+      echo -n -e "\nwaiting for processes to exit";
+      sleep 1
+      let count=$count+1;
+    done
+
+    if [ $count -gt $kwait ]; then
+      echo -n -e "\nkilling processes which didn't stop after $SHUTDOWN_WAIT seconds"
+      kill -9 $pid
+      echo  " \nprocess killed manually"
+    fi
+  else
+    echo "Tomcat is not running"
+  fi
+
+  return 0
+}
+pid=$(tomcat_pid)
+
+ if [ -n "$pid" ]
+  then
+    echo "Tomcat is running with pid: $pid"
+    stop
+  else
+    echo "Tomcat is not running"
+    start
+  fi
+exit 0
