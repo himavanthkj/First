7141614ff89488f10cccc05aed0e7fb3b32a19ce
himavanthkj@gmail.com <himavanthkj@gmail.com>
2019-08-01 01:51:44 -0400

    Adding crucible chages

--- tomcatrestart.sh
+++ tomcatrestart.sh
@@ -13,6 +13,8 @@ hostip=`hostname -I`
 ps -ef | grep "org.apache.catalina.startup.Bootstrap" | grep -v grep  > xyz.txt
 tomcat_pid=`awk '{print $2}' xyz.txt`
 echo $tomcat_pid
+FMT=`cd /home/hima/GIT/First/ | stat -c %y tomcatrestart1.sh | cut -d' ' -f2 | awk -F ':' '{print $2}'`
+curtim=`date +'%M'`
 
 #Tomcat_pid=$(tomcat_pid)
 if [ -n "$tomcat_pid" ]
@@ -25,17 +27,20 @@ then
 		echo "Url as well loading fine with (status: $urlstatus)"
 	else
 		echo -e "URL is not loading, so will kill the (pid: $tomcat_pid) and start the tomcat"
-		kill -9 $tomcat_pid
-		sh /opt/tomcat/latest/bin/startup.sh
-		wait $! #It will let wait the next command until previous one completes
-		ps -ef | grep "org.apache.catalina.startup.Bootstrap" | grep -v grep  > xyz.txt
-		status_pid=`awk '{print $2}' xyz.txt`	
-       		if [ -n "$status_pid" ]
-        	then
-        		echo "Tomcat is running (pid: $status_pid) after a restart"
-        	else
-       			echo "Please investigate as tomcat didnt start through script"
-        	fi
+                kill -9 $tomcat_pid
+		if [ "$FMT" != "$curtim" ]
+                then
+                        echo "Currently its not backing up, hence going ahead with service restart"
+			sh /opt/tomcat/latest/bin/startup.sh
+			wait $! #It will let wait the next command until previous one completes
+			ps -ef | grep "org.apache.catalina.startup.Bootstrap" | grep -v grep  > xyz.txt
+			status_pid=`awk '{print $2}' xyz.txt`	
+       			if [ -n "$status_pid" ]
+        		then
+        			echo "Tomcat is running (pid: $status_pid) after a restart"
+        		else
+       				echo "Please investigate as tomcat didnt start through script"
+        		fi
 	fi
 else
         echo "Starting Tomcat as its down"
